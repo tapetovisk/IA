929d0609aaca2fa9f2e613e537ba15e563164b1f
Anderson Matos Nascimento <anderson@gi.com.br>
2026-05-19 09:43:05 -0300

    Adiciona provedores Anthropic e Microsoft Foundry
    Inclui referências aos pacotes Anthropic.Foundry e Microsoft.Agents.AI.Foundry. Implementa as classes e interfaces ProvedorAnthopic e ProvedorMicrosoftFoundry, permitindo configuração e integração com os respectivos provedores de IA.

---
+++ Service.IA/Provedor/Interface/IProvedorAnthopic.cs
@@ -0,0 +1,9 @@
+﻿using Service.IA.Provedor.Base;
+
+namespace Service.IA.Provedor.Interface
+{
+    public interface IProvedorAnthopic : IProvedorBase
+    {
+        IProvedorBase SetProvedor(string url, string apiKey);
+    }
+}
---
+++ Service.IA/Provedor/ProvedorMicrosoftFoundry.cs
@@ -0,0 +1,30 @@
+﻿using Azure.AI.Projects;
+using Azure.Identity;
+using OpenAI;
+using Service.IA.Provedor.Base;
+using Service.IA.Provedor.Interface;
+using System.ComponentModel;
+
+namespace Service.IA.Provedor
+{
+    public class ProvedorMicrosoftFoundry : ProvedorBase, IProvedorMicrosoftFoundry
+    {
+
+        [Description("Provedor Microsoft Foundry")]
+        public IProvedorBase SetProvedor(
+            [Description("URL do serviço Microsoft Foundry.")] string url,
+            [Description("Chave de API do Microsoft Foundry.")] string apiKey)
+        => base.SetProvedor(url, new Tuple<string, string>(TagKey, apiKey), 10);
+
+        internal override OpenAIClient SetProvedor()
+        {
+            var Foundry = new AIProjectClient(
+                new Uri(base.url),
+                new DefaultAzureCredential());
+
+            openAIClient = Foundry.ProjectOpenAIClient;
+
+            return openAIClient;
+        }
+    }
+}
